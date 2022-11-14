18ce013fe7100b27bb24d162f8c12a929ba90f7d
ArtemPavlov72 <pavlov.tema@gmail.com>
2022-11-14 23:12:19 +0500

    Initial Commit

---
+++ MyTasksAndTests/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyTasksAndTests
+//
+//  Created by Артем Павлов on 14.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
