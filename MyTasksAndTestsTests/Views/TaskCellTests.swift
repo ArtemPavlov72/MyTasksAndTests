58dcab247f111d386170a4757100d161a4782502
ArtemPavlov72 <pavlov.tema@gmail.com>
2022-11-27 12:29:39 +0500

    1. TaskCellTests updated with new tests. 2. To Task Cell added configure methods. 3. Created DetailViewControllerTests.

--- MyTasksAndTestsTests/Views/TaskCellTests.swift
+++ MyTasksAndTestsTests/Views/TaskCellTests.swift
@@ -13,6 +13,7 @@ class TaskCellTests: XCTestCase {
     var cell: TaskCell!
 
     override func setUpWithError() throws {
+        //мы создавали ячейку через сториборд, поэтому работаем с ним
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
         let controller = storyboard.instantiateViewController(withIdentifier: String(describing: TaskListViewController.self)) as! TaskListViewController
         controller.loadViewIfNeeded()
@@ -31,33 +32,86 @@ class TaskCellTests: XCTestCase {
     //ячейка должна отображать какую-нибудь информацию
     //чтобы отображать информацию, нам пригодятся ярлыки
     //проверим, есть ли у ячейки заголовок (свойство titleLabel)
-    
     func testCellHasTitleLabel() {
-        
-        //мы создавали ячейку через сториборд, поэтому работаем с ним
-        
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        let controller = storyboard.instantiateViewController(withIdentifier: String(describing: TaskListViewController.self)) as! TaskListViewController
-        controller.loadViewIfNeeded()
-        
-        let tableView = controller.tableView
-        let dataSource = FakeDataSource()
-        tableView?.dataSource = dataSource
-        
-        let cell = tableView?.dequeueReusableCell(withIdentifier: String(describing: TaskCell.self), for: IndexPath(row: 0, section: 0)) as! TaskCell
-        
         XCTAssertNotNil(cell.titleLabel)
-        
     }
     
     //проверяем действительно ли наш titleLabel находится внутри view
-    
     func testCellHasTitleLabelInContentView() {
-      
-        
         XCTAssertTrue(cell.titleLabel.isDescendant(of: cell.contentView))
     }
+    
+    func testCellHasLocationLabel() {
+        XCTAssertNotNil(cell.locationLabel)
+    }
+    
+    func testCellHasLocationLabelInContentView() {
+        XCTAssertTrue(cell.locationLabel.isDescendant(of: cell.contentView))
+    }
+    
+    func testCellHasDateLabel() {
+        XCTAssertNotNil(cell.dateLabel)
+    }
+    
+    func testCellHasDateLabelInContentView() {
+        XCTAssertTrue(cell.dateLabel.isDescendant(of: cell.contentView))
+    }
+    
+    // тесты конфигурации ячейки
+    
+    func testConfigureSetsTitle() {
+        let task = Task(title: "Foo")
+        cell.configure(withTask: task)
+        XCTAssertEqual(cell.titleLabel.text, task.title)
+    }
+    
+    func testConfigureSetsDate() {
+        let task = Task(title: "Foo")
+        cell.configure(withTask: task)
+        
+        let df = DateFormatter()
+        df.dateFormat = "dd.MM.yy"
+        let date = task.date
+        let dateString = df.string(from: date!)
+        
+        XCTAssertEqual(cell.dateLabel.text, dateString)
+    }
+    
+    func testConfigureLocation() {
+        let location = Location(name: "Foo")
+        
+        let task = Task(title: "Bar", location: location)
+        cell.configure(withTask: task)
+        
+        XCTAssertEqual(cell.locationLabel.text, task.location?.name)
+    }
+    
+    //проверяем, что выполненные задачи - зачеркнуты
+    private func configureCellWithTask() {
+        let task = Task(title: "Foo")
+        cell.configure(withTask: task, done: true)
+    }
+    
+    func testDoneTaskShouldStrikeThrought() {
+        configureCellWithTask()
+        
+        //меняем стиль отображения
+        let attributedString = NSAttributedString(string: "Foo", attributes: [NSAttributedString.Key.strikethroughStyle: NSUnderlineStyle.single.rawValue])
+        
+        XCTAssertEqual(cell.titleLabel.attributedText, attributedString)
+    }
+    
+    //проверяем, что у выполненной задачи лэйбел с датой равен nil
+    func testDoneTaskDateLabelEqualsNil() {
+        configureCellWithTask()
+        XCTAssertNil(cell.dateLabel)
+    }
 
+    //проверяем, что у выполненной задачи лэйбел с location равен nil
+    func testDoneTaskLocationLabelEqualsNil() {
+        configureCellWithTask()
+        XCTAssertNil(cell.locationLabel)
+    }
 }
 
 extension TaskCellTests {
