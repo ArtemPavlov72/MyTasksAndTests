f67d94275598133968dc0be9413a98fdcddefb02
ArtemPavlov72 <pavlov.tema@gmail.com>
2022-11-28 00:10:19 +0500

    NewTaskViewControllerTests created. Added new tests for NewTaskViewController.

---
+++ MyTasksAndTests/ViewControllers/NewTaskViewController.swift
@@ -0,0 +1,43 @@
+//
+//  NewTaskViewController.swift
+//  MyTasksAndTests
+//
+//  Created by Артем Павлов on 27.11.2022.
+//
+
+import UIKit
+import CoreLocation
+
+class NewTaskViewController: UIViewController {
+    
+    var taskManager: TaskManager!
+    var dateFormatter: DateFormatter {
+        let df = DateFormatter()
+        df.dateFormat = "dd.MM.yy"
+        return df
+    }
+    var geocoder = CLGeocoder()
+
+    @IBOutlet var titleTextField: UITextField!
+    @IBOutlet var locationTextField: UITextField!
+    @IBOutlet var dateTextField: UITextField!
+    @IBOutlet var adressTextField: UITextField!
+    @IBOutlet var descriptionTextField: UITextField!
+    @IBOutlet var saveButton: UIButton!
+    @IBOutlet var cancelButton: UIButton!
+    
+    @IBAction func save() {
+        let titleString = titleTextField.text
+        let locationString = locationTextField.text
+        let date = dateFormatter.date(from: dateTextField.text!)
+        let descriptionString = descriptionTextField.text
+        let addressString = adressTextField.text
+        geocoder.geocodeAddressString(addressString!) { [unowned self] placemarks, error in
+            let placemark = placemarks?.first
+            let coordinate = placemark?.location?.coordinate
+            let location = Location(name: locationString!, coordinate: coordinate)
+            let task = Task(title: titleString!, description: descriptionString, date: date, location: location)
+            self.taskManager.add(task: task)
+        }
+    }
+}
